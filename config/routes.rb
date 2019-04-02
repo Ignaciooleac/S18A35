5407e1ed7b79ec4e73f168a5d45de011bf37dd71
igacio <ignacio.olea.c@gmail.com>
2019-04-02 19:56:28 -0300

    secondo commito

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'products#index'
+  get 'orders/create'
+ 
+  resources :products do  
+    resources :orders
+  end
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  } 
+  resources :orders, only: :index
+
+   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
